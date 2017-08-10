b1c9ed93bc290f28f1a126f210ec6f484af282ff
Chas Threlkeld <chathrel@indiana.edu>
2017-08-10 18:22:19 -0500

    Initial commit

---
+++ plot2.R
@@ -0,0 +1,3 @@
+#' Have total emissions from PM2.5 decreased in Baltimore City, Maryland
+#' (fips == "24510") from 1999 to 2008? Use the base plotting system to make
+#' a plot answering this question.
